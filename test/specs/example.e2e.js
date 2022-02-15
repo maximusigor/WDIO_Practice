ccb2afc2b01407d73b02c744e3851c9fe4333de3
maximusigor <shilov0209@gmail.com>
2022-02-15 11:05:39 +0200

    changes

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -4,13 +4,12 @@ const SecurePage = require('../pageobjects/secure.page');
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
-
         await LoginPage.login('Ihor', 'password');
-        await browser.pause(10000)
+        await browser.pause(10000);
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
-        await browser.pause(10000)
+        await browser.pause(10000);
     });
 });
 
